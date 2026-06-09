8856ffd7ab837bd72c548797168339dc8ecd5f15
Krushna Yerawar <krushnayerawar2004@gmail.com>
2026-06-09 10:24:05 +0530

    Implement seatbelt alarm system in cpp_modify.cpp

---
+++ cpp_modify.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool alarm_triggered = false;
+ 
+void trigalarm() {
+    cout << "WARNING: Seatbelt missing!" << endl;
+    alarm_triggered = true;
+}
+
+int main() {
+    int car_speed = 90;
+    int seatbelt = 0; // 0 matlab seatbelt nahi pehni
+
+    if (car_speed > 80 && seatbelt == 0) {
+        trigalarm();
+    } else {
+        cout << "Normal condition" << endl;
+    }
+
+    if (alarm_triggered == true) {
+        cout << "TEST PASSED: Alarm working fine!" << endl;
+        return 0; // GitHub ko batayega ki test PASS ho gaya
+    } else {
+        cout << "TEST FAILED!" << endl;
+        return 1; // GitHub ko batayega ki test FAIL ho gaya
+    }
+}
